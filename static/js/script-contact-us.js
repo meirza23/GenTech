91be589b1a85b5783bfaca32dcd9ecd79bbd354e
meirza23 <mirzaetka23@gmail.com>
2024-07-30 01:30:32 +0300

    fin

--- static/js/script-contact-us.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const buttonMainMenu = document.querySelector(
-  ".frame-3-removebg-preview-1-icon"
-);
-
-buttonMainMenu.addEventListener("click", function () {
-  window.location.href = "welcome";
-});
